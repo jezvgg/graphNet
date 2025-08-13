8ca9458448c4187687a54723de2457d6fb8cbde9
jezv <jezv@yandex.ru>
2025-08-13 10:27:32 +0800

    #49 Fix Smal fix to predict node

--- Src/Nodes/predict_node.py
+++ Src/Nodes/predict_node.py
@@ -11,9 +11,4 @@ class PredictNode(ParameterNode):
 
 
     def compile(self):
-        with dpg.window(label="Предсказание", modal=True, no_close=True) as popup:
-            dpg.add_loading_indicator(width=100, height=100)
-
         super().compile(kwargs={"verbose": False})
-
-        dpg.delete_item(popup)
