503821703396ceb5bffc3cb411695ea00948fd7a
Albert Nosachenko <150217988+mumok4@users.noreply.github.com>
2025-08-16 14:40:08 +0800

    #76, waiting review, added dpg types used in app
    Добавлены типы, которые мы используем(потенциально можем использовать) в приложении
    
    Список типов может расширяться, но основные, пока что, включены

---
+++ Src/Enums/dpg_types.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+
+
+
+class DPGType(Enum):
+    """
+        Enum для элементов dearpygui
+    """
+    # Основные элементы
+    BUTTON = 'mvAppItemType::mvButton'
+    INPUT_TEXT = 'mvAppItemType::mvInputText'
+    INPUT_INT = 'mvAppItemType::mvInputInt'
+    INPUT_FLOAT = 'mvAppItemType::mvInputFloat'
+    CHECKBOX = 'mvAppItemType::mvCheckbox'
+    RADIO_BUTTON = 'mvAppItemType::mvRadioButton'
+    TEXT = 'mvAppItemType::mvText'
+    COMBO = 'mvAppItemType::mvCombo'
+    LISTBOX = 'mvAppItemType::mvListbox'
+    SLIDER_INT = 'mvAppItemType::mvSliderInt'
+    SLIDER_FLOAT = 'mvAppItemType::mvSliderFloat'
+    IMAGE = 'mvAppItemType::mvImage'
+    PROGRESS_BAR = 'mvAppItemType::mvProgressBar'
+    COLOR_PICKER = 'mvAppItemType::mvColorPicker'
+
+    # Контейнеры
+    WINDOW_APP_ITEM = 'mvAppItemType::mvWindowAppItem'
+    CHILD_WINDOW = 'mvAppItemType::mvChildWindow'
+    GROUP = 'mvAppItemType::mvGroup'
+    TAB_BAR = 'mvAppItemType::mvTabBar'
+    MENU_BAR = 'mvAppItemType::mvMenuBar'
+    TOOLTIP = 'mvAppItemType::mvTooltip'
+    COLLAPSING_HEADER = 'mvAppItemType::mvCollapsingHeader'
+
+    # Редактор узлов
+    NODE_EDITOR = 'mvAppItemType::mvNodeEditor'
+    NODE = 'mvAppItemType::mvNode'
+    NODE_LINK = 'mvAppItemType::mvNodeLink'
+
+    # Обработчики
+    HANDLER_REGISTRY = 'mvAppItemType::mvHandlerRegistry'
+    FONT_REGISTRY = 'mvAppItemType::mvFontRegistry'
