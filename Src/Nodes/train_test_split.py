182cea9c6fd0bb5f104ed65b87eb1d9fde9e2033
Dmitriy <babygreenyoda@yandex.ru>
2026-03-17 13:58:15 +0800

    #56 Add, train_test_split compile and shuffle suppor

--- Src/Nodes/train_test_split.py
+++ Src/Nodes/train_test_split.py
@@ -8,7 +8,7 @@ from Src.Nodes import DataNode
 class TrainTestSplitNode(DataNode):
     theme_name: Themes = Themes.UTILS
     @staticmethod
-    def split(x: np.ndarray, y: np.ndarray, test_size: float = 0.25, train_size:float | None = None,**kwargs):
+    def split(x: np.ndarray, y: np.ndarray, test_size: float = 0.25, train_size:float | None = None, random_state: int | None = None, **kwargs):
         x.assaray = np.asarray(x) 
         y.assaray = np.asarray(y) if y is not None else None
         if x.ndim ==0:
@@ -22,16 +22,26 @@ class TrainTestSplitNode(DataNode):
                 return size
             else:
                 raise ValueError("Размер должен быть числом с плавающей запятой или целым числом")
+            
         n_samples = x.shape[0]
         test_size = _size_to_int(test_size,n_samples) if test_size is not None else n_samples - train_size
         train_size = _size_to_int(train_size,n_samples) if train_size is not None else n_samples - test_size
+
         if train_size is None:
             train_size = n_samples -test_size
         if test_size is None:
             test_size = n_samples - train_size
         else:
             raise ValueError("Неккоректные размеры тестовой и обучающей выборки")
+        indices = np.arange(n_samples)
+        rng = np.random.default_rng(random_state)
+        rng.shuffle(indices)
         
+        n_train = indices[:train_size]
+        n_test = indices[train_size:train_size+test_size]
+        X_train, X_test = x[n_train], x[n_test]
+        y_train, y_test = y[n_train], y[n_test]
+        return X_train, X_test, y_train, y_test if y is not None else X_train, X_test
 
         
 
