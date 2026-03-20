a597e0a8c286910792a7668040b1e582440cc7f2
Turik <turalsuleymanov2006@gmail.com>
2026-03-20 16:10:31 +0800

    #30 WIP Implement open_audio_data method

--- Src/Nodes/shape_node.py
+++ Src/Nodes/shape_node.py
@@ -3,6 +3,7 @@ from abc import abstractmethod
 
 import numpy as np
 import keras
+import librosa
 
 from Src.Enums import Themes
 from Src.Utils import Backfield
@@ -47,3 +48,32 @@ class ShapeNode(DataNode):
 
         return np.array(images)
     
+
+    @staticmethod
+    def open_audio_data(files: str, NFFT: int = 1024, noverlap: int = 512, max_duration_sec: float = 5.0, *args, **kwargs):
+        if not files:
+            raise AttributeError("Вы не выбрали данные, которые нужно открыть!")
+        
+        audios = []
+        extensions = {'.wav', '.mp3', '.flac', '.ogg', '.m4a'}
+        
+        for audio_path in sorted(Path(files).iterdir()):
+            if audio_path.is_file() and audio_path.suffix.lower() in extensions:
+            
+                data, sample_rate = librosa.load(str(audio_path), sr=None, mono=True, duration=max_duration_sec)
+                    
+                nfft = NFFT if NFFT else int(2 ** round(np.log2(sample_rate * 0.025)))
+                noverlap = noverlap if noverlap else nfft // 2
+                hop_length = nfft - noverlap 
+                    
+                stft_matrix = librosa.stft(data, n_fft=nfft, hop_length=hop_length)
+                
+                spectrum = np.abs(stft_matrix)**2
+                
+                spectrum = 10. * np.log10(spectrum + 1e-10)
+                audios.append(spectrum)
+
+        if not audios:
+            return np.array([])
+
+        return audios[0]
