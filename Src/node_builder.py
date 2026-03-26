ca6a5373eac98a8c67a07ab64d458842ede95cde
Dest1n640 <realmadridfunca@gmail.com>
2026-03-26 14:49:06 +0800

    #95 WIP: Refactor list node UI, fix title colors via ThemeManager and add group drag_payload

--- Src/node_builder.py
+++ Src/node_builder.py
@@ -14,6 +14,8 @@ from Src.Managers import ThemeManager
 BUTTON_HEIGHT: int = 32
 LINE_HEIGHT: int = 30
 PADDING: int = 8
+LIST_HEADER_WIDTH: int = 210
+LIST_FIELD_WIDTH: int = 120
 
 class NodeBuilder:
     '''
@@ -58,11 +60,11 @@ class NodeBuilder:
                         with dpg.tree_node(label=subanchor) as tree_subanchor:
 
                             for node in self.node_list[anchor][subanchor]:
-                              self.build_list_node(node_data=node, parent=tree_subanchor)
+                              self._build_list_node(node_data=node, parent=tree_subanchor)
 
         return list
 
-    def build_list_node(self, node_data: NodeAnnotation, parent: int | str) -> int | str:
+    def _build_list_node(self, node_data: NodeAnnotation, parent: int | str) -> int | str:
       '''
       Построение элемента списка нод, визуально имитирующего ноду в редакторе.
 
@@ -74,47 +76,47 @@ class NodeBuilder:
           int | str - идентификатор созданной группы
       '''
 
+
       visible_params: list[tuple[str, Parameter]] = [
           (label, param) for label, param in node_data.annotations.items()
           if label != 'INPUT' and not isinstance(param.hint, ANode)
       ]
 
-      extra_lines: int = 1
-      if node_data.input:
-          extra_lines += 1
-      if node_data.output:
-          extra_lines += 1
-
-      height: int = BUTTON_HEIGHT + (len(visible_params) + extra_lines) * LINE_HEIGHT + PADDING
-
       with dpg.group(horizontal=False, parent=parent, user_data=node_data) as group:
-        # with dpg.drag_payload(parent=group, drag_data=node_data):
-        #     dpg.add_text(node_data.label)
+        with dpg.drag_payload(parent=group, drag_data=group):
+            dpg.add_text(node_data.label)
 
-        btn: int | str = dpg.add_button(label=node_data.label, width=30)
-        ThemeManager.apply_theme(btn, node_data.node_type.theme_name)
-        with dpg.drag_payload(parent=btn, drag_data=group):
-          dpg.add_text(node_data.label)
+        header_button: int | str = dpg.add_button(
+            label=node_data.label,
+            width=LIST_HEADER_WIDTH
+        )
+
+        layer_theme = ThemeManager._themes_config[node_data.node_type.theme_name]
+        title_color = layer_theme["mvNode"]["mvNodeCol_TitleBar"]
+
+        with dpg.theme() as btn_theme:
+            with dpg.theme_component(dpg.mvButton):
+                dpg.add_theme_color(dpg.mvThemeCol_Button, title_color)
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, title_color)
+        dpg.bind_item_theme(header_button, btn_theme)
 
         if node_data.input:
             dpg.add_text("INPUT", indent=4)
-            # input_label: int | str = dpg.add_text("INPUT", indent=4)
-            # with dpg.drag_payload(parent=input_label, drag_data=group):
-            #     dpg.add_text(node_data.label)
+        
+        with dpg.tree_node(label="Docs", indent=4) as docs_node:
+          dpg.add_text(node_data.docs, wrap=LIST_HEADER_WIDTH - 20)
 
-        with dpg.tree_node(label="Docs") as docs_node:
-            dpg.add_text(node_data.docs)
-            # with dpg.drag_payload(parent=docs_node, drag_data=group):
-            #     dpg.add_text(node_data.label)
         
         for label, param in visible_params:
-            param.hint.build(label=label, parent=group, enabled=False)
+              with dpg.group(horizontal=True) as param_group:
+                param.hint.build(label=label,
+                                parent=group,
+                                width=LIST_FIELD_WIDTH,
+                                enabled=False)
+
 
         if node_data.output:
-            dpg.add_text("Output", indent=4)
-          # output_label: int | str = dpg.add_text("OUTPUT", indent=4)
-          # with dpg.drag_payload(parent=output_label, drag_data=group):
-          #     dpg.add_text(node_data.label)
+          dpg.add_text("OUTPUT", indent=4)
 
       return group
 
