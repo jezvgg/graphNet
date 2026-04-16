01b4840e7666ec6913255ff3b5ab294e14c0767b
Turik <turalsuleymanov2006@gmail.com>
2026-04-16 10:48:27 +0800

    #143 Done: Add figure field to DataNode for storing plots

--- Src/Nodes/data_node.py
+++ Src/Nodes/data_node.py
@@ -1,7 +1,8 @@
 from Src.Enums import Themes
 from Src.Nodes import AbstractNode
 
-
+from Src.Utils import Backfield
 
 class DataNode(AbstractNode):
     theme_name: Themes = Themes.DATA
+    figure: Backfield = Backfield()
