95d1375f737bcaea58512c3a4e6a2d0d5b43ed14
Dest1n640 <realmadridfunca@gmail.com>
2026-06-22 09:39:59 +0800

    #115 WIP: add serialize_project method for project serialization

--- Src/Utils/serialization/serializers.py
+++ Src/Utils/serialization/serializers.py
@@ -9,11 +9,14 @@ from Src.Enums.attr_type import AttrType
 from Src.Config.Annotations import ANode
 
 
+
+
 class ProjectEncoder(json.JSONEncoder):
     """
     Кастомный JSON-энкодер, инкапсулирующий в себе всю логику
     сериализации сложных и неизвестных типов данных.
     """
+
 
     @singledispatchmethod
     def serialize(self, obj: any) -> any:
@@ -24,12 +27,14 @@ class ProjectEncoder(json.JSONEncoder):
         """
         return obj
 
+
     @serialize.register(Path)
     def _serialize_path(self, obj: Path) -> str:
         """
         Преобразует объект pathlib.Path в строку.
         """
         return str(obj)
+
 
     @serialize.register(enum.Enum)
     def _serialize_enum(self, obj: enum.Enum) -> str:
@@ -38,12 +43,14 @@ class ProjectEncoder(json.JSONEncoder):
         """
         return obj.value
 
+
     @serialize.register(tuple)
     def _serialize_tuple(self, obj: tuple) -> list:
         """
         Рекурсивно сериализует элементы кортежа и упаковывает их в список.
         """
         return [self.serialize(item) for item in obj]
+
 
     @serialize.register(AbstractNode)
     def _serialize_node(self, node: AbstractNode) -> dict:
@@ -74,6 +81,7 @@ class ProjectEncoder(json.JSONEncoder):
             "parameters": param_values
         }
 
+
     def default(self, obj: any) -> any:
         """
         Переопределенный стандартный метод JSON-энкодера.
@@ -82,3 +90,45 @@ class ProjectEncoder(json.JSONEncoder):
             return self.serialize(obj)
         except TypeError:
             return super().default(obj)
+    
+
+def serialize_project(nodes: list[AbstractNode]) -> dict:
+    """
+    Сериализует весь холст: преобразует граф узлов и связей
+    в абстрактный JSON-совместимый формат, решая проблему динамических UUID.
+    """
+    node_tag_to_id = {node.node_tag: f"node_{i}" for i, node in enumerate(nodes)}
+    
+    serialized_nodes = []
+    serialized_links = []
+
+    encoder = ProjectEncoder()
+
+    for node in nodes:
+        node_data = encoder.serialize(node)
+
+        node_data["id"] = node_tag_to_id(node.node_tag)
+        serialized_nodes.append(node_data)
+        for attr_incoming, attr_outgoing_list in node.incoming.items():
+
+            receiver_pin_label = dpg.get_item_label(attr_incoming)
+
+            for attr_outgoing in attr_outgoing_list:
+                receiver_pin_label = dpg.get_item_label(attr_incoming)
+                for attr_outgoing in attr_outgoing_list:
+                    sender_node_tag = dpg.get_item_parent(attr_outgoing)
+                    sender_node = dpg.get_item_user_data(sender_node_tag)
+                    sender_pin_label = dpg.get_item_label(attr_outgoing)
+
+                    link_data = {
+                        "sender_node_id": node_tag_to_id[sender_node.node_tag],
+                        "sender_pin": sender_pin_label,
+                        "receiver_node_id": node_tag_to_id[node.node_tag],
+                        "receiver_pin": receiver_pin_label
+                    }
+                    serialized_links.append(link_data)
+
+    return {
+        "nodes": serialized_nodes,
+        "links": serialized_links
+    }         
