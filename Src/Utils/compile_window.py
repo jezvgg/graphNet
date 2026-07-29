6a907a234bba4531d7628c6f352748ffaa9bc2cd
pola0104 <pola0111.0444@gmail.com>
2026-07-29 12:23:38 +0800

    #137 WIP Prescribed methods for creating a window, displaying and deleting logs

--- Src/Utils/compile_window.py
+++ Src/Utils/compile_window.py
@@ -1,11 +1,27 @@
 from Src.Utils.singleton import singleton
 from Src.Utils.viewport import on_viewport_resize_callback
+import dearpygui.dearpygui as dpg
 @singleton
-class CompileWondow():
-    def __init__(self, max_lines: int  = 10):
+class CompileWindow():
+    def __init__(self, max_lines: int = 7):
         self.logs = [] #все логи
         self.max_lines = max_lines #максимальная вместимость в окне
-        self.window = None #окно 
-        self.log_text = None #текст
+        self.log_text_item = None
+        self.log_text = "" #текст
+    def create_window(self): #создание окна
+        with dpg.window(label="Компиляция графа", modal=True, no_title_bar=True, no_resize=True, no_move=True, tag="compile_window") as window:
+            self.log_text_item= dpg.add_text(self.log_text)
+            dpg.add_button(label="Close", callback=lambda: dpg.configure_item(window, show=False))
+
+        on_viewport_resize_callback()
+    def push(self, text): #добавление текста
+        self.logs.insert(0, text)
+        self.pop()
+        self.log_text = "\n".join(self.logs)
+        dpg.set_value(self.log_text_item, self.log_text)
+    def pop(self): 
+        if len(self.logs) > self.max_lines:
+            self.logs.pop()
+
 
     
