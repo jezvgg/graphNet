57908d61fe05c64e66c06d3d335fb75f2c282717
pola0104 <pola0111.0444@gmail.com>
2026-08-04 17:21:01 +0800

    #137 WIP Auto-resize compile window to fit log content

--- Src/Utils/compile_window.py
+++ Src/Utils/compile_window.py
@@ -8,15 +8,20 @@ class CompileWindow():
         self.max_lines = max_lines #максимальная вместимость в окне
         self.log_text_item = None
     def create_window(self): #создание окна
-        with dpg.window(label="Компиляция графа", modal=True, no_resize=True, no_move=True, no_scrollbar=True,width=500,height=115, tag="compile_window") as window:
+        with dpg.window(label="Компиляция графа", modal=True, no_resize=True, no_move=True, no_scrollbar=True,width=500, tag="compile_window") as window:
             self.log_text_item= dpg.add_text("", wrap=460)
-            dpg.add_button(label="Close", callback=lambda: dpg.configure_item(window, show=False))
+            dpg.add_button(label="Закрыть", callback=lambda: dpg.configure_item(window, show=False))
 
         on_viewport_resize_callback()
     def push(self, text): #добавление текста
         self.logs.insert(0, text)
         self.pop()
         dpg.set_value(self.log_text_item, "\n".join(self.logs))
+        dpg.split_frame()
+        w, h = dpg.get_item_rect_size(self.log_text_item)
+        dpg.configure_item("compile_window", height=h + 75)
+        dpg.split_frame()
+        on_viewport_resize_callback()
     def pop(self): 
         if len(self.logs) > self.max_lines:
             self.logs.pop()
