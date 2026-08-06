1f659bed32ab812c29b37ae34bdceebb23693275
pola0104 <pola0111.0444@gmail.com>
2026-08-06 22:13:11 +0800

    #137 WIP Rework compile window to per-line log items with error highlighting

--- Src/Utils/compile_window.py
+++ Src/Utils/compile_window.py
@@ -4,27 +4,47 @@ import dearpygui.dearpygui as dpg
 @singleton
 class CompileWindow():
     def __init__(self, max_lines: int = 7):
-        self.logs = [] #все логи
+        self.log_items = [] #все логи
         self.max_lines = max_lines #максимальная вместимость в окне
-        self.log_text_item = None
+        self.log_group = None
+
     def create_window(self): #создание окна
-        with dpg.window(label="Компиляция графа", modal=True, no_resize=True, no_move=True, no_scrollbar=True,width=500, tag="compile_window") as window:
-            self.log_text_item= dpg.add_text("", wrap=460)
+        with dpg.window(label="Компиляция графа", modal=True, no_resize=True, no_move=True, no_scrollbar=True,width=500,height=300, tag="compile_window") as window:
+            with dpg.group() as self.log_group:
+                pass
             dpg.add_button(label="Закрыть", callback=lambda: dpg.configure_item(window, show=False))
 
         on_viewport_resize_callback()
-    def push(self, text): #добавление текста
-        self.logs.insert(0, text)
+    
+    def push(self, text, flag: bool = True):
+        children = dpg.get_item_children(self.log_group, slot=1)
+        first_child = children[0] if children else None
+        if flag:
+            item = dpg.add_text(text, wrap=460, parent=self.log_group,color=[255, 255, 255, 255])
+        else:
+            item = dpg.add_text(text, wrap=460, parent=self.log_group,color=[239, 83, 80, 255])
+        if first_child:
+            dpg.move_item(item, parent=self.log_group, before=first_child)
+        self.log_items.append(item)
         self.pop()
-        dpg.set_value(self.log_text_item, "\n".join(self.logs))
-        dpg.split_frame()
-        w, h = dpg.get_item_rect_size(self.log_text_item)
-        dpg.configure_item("compile_window", height=h + 75)
-        dpg.split_frame()
-        on_viewport_resize_callback()
+
     def pop(self): 
-        if len(self.logs) > self.max_lines:
-            self.logs.pop()
+        if len(self.log_items) > self.max_lines:
+            dpg.delete_item(self.log_items.pop(0))
+
+"""
+    def push_error(self, text):
+        children = dpg.get_item_children(self.log_group, slot=1)
+        first_child = children[0] if children else None
+        item = dpg.add_text(text, wrap=460, parent=self.log_group,
+                            color=[239, 83, 80, 255])
+        if first_child:
+            dpg.move_item(item, parent=self.log_group, before=first_child)
+        self.log_items.append(item)
+        self.pop()
+"""
+
+
 
 
     
