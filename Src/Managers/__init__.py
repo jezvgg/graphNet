ec62503d9fb86fb9fc0fec35522f936657158dea
Turik06 <turalsuleymanov2006@gmail.com>
2026-09-06 23:25:34 +0800

    #151 WIP Export ExtensionManager in Managers module

--- Src/Managers/__init__.py
+++ Src/Managers/__init__.py
@@ -2,3 +2,4 @@ from .event_manager import EventManager
 from .theme_manager import ThemeManager
 from .font_manager import FontManager
 from .size_manager import SizeManager
+from .extension_manager import ExtensionManager
